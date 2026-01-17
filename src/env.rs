1ee5d5cab632940c7a58b24d47e296786528f971
bit-aloo <sshourya17@gmail.com>
2026-01-18 00:18:45 +0530

    add backtrack

--- src/env.rs
+++ src/env.rs
@@ -2,15 +2,10 @@ use std::collections::HashMap;
 
 use crate::val::Val;
 
-
-
-
-
 #[derive(Debug, PartialEq, Default)]
 pub(crate) struct Env {
-    bindings: HashMap<String, Val>
+    bindings: HashMap<String, Val>,
 }
-
 
 impl Env {
     pub(crate) fn store_binding(&mut self, name: String, val: Val) {
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-pub mod utils;
-pub mod expr;
 pub mod binding_def;
 pub mod env;
+pub mod expr;
+pub mod utils;
 pub mod val;
